e23c594f232a64f5c3bc704147e53882a83acab4
themmfuad <123576984+themmfuad@users.noreply.github.com>
2025-06-07 20:14:49 +0600

    completed the log-archive.sh according to the requirements in the project

--- log-archive.sh
+++ log-archive.sh
@@ -1,5 +1,15 @@
+#storing the argument to a variable
 LOG_DIR=$1
 ARCHIVE_DIR=$2
-CTIME=$(date +"%Y%m%d_%H%M%S")
 
-echo $CTIME
+#create the archive directory if it doesn't exit
+mkdir -p $ARCHIVE_DIR
+
+#store the archive filename with the current system time in a varible
+FNAME="log_archive_"$(date +"%Y%m%d_%H%M%S")".tar.gz"
+
+#create the archive file
+tar -cvzf "$FNAME" $LOG_DIR
+
+#move the archive file to the destination directory
+mv $FNAME $ARCHIVE_DIR
